affbd9a9bfa62745e51bd6f75887886899d142cc
Benoit Triay <benoit.tria+13y@gmail.com>
2022-12-12 21:58:29 +0000

    HeroicCreature constructor

--- src/character/HeroicCreature.cpp
+++ src/character/HeroicCreature.cpp
@@ -1,1 +1,108 @@
 module HeroicCreature;
+
+HeroicCreature::HeroicCreature(Race race) : m_race(race)
+{
+	// main races
+	switch (race)
+	{
+	case (Race::HillDwarf):
+	case (Race::MountainDwarf):
+		HeroicCreature(1, 60, Size::medium, 25);
+		set_ability_score_increase(Ability::constitution, 2);
+		add_resistance(Damage::poison);
+		break;
+
+	case (Race::Dragonborn):
+		HeroicCreature(1, 0, Size::medium, 30);
+		set_ability_score_increase(Ability::charisma, 1);
+		set_ability_score_increase(Ability::strength, 2);
+
+	case (Race::HighElf):
+	case (Race::WoodElf):
+	case (Race::DarkElf):
+		HeroicCreature(1, 60, Size::medium, 30);
+		set_ability_score_increase(Ability::dexterity, 2);
+		break;
+
+	case (Race::ForestGnome):
+	case (Race::RockGnome):
+		HeroicCreature(1, 60, Size::small, 25);
+		set_ability_score_increase(Ability::intelligence, 2);
+		break;
+
+	case (Race::HalfElf):
+		HeroicCreature(1, 60, Size::medium, 30);
+		set_ability_score_increase(Ability::charisma, 2);
+		// + 2 abilities chosen by the player (+1 each)
+		break;
+
+	case (Race::Lightfoot):
+	case (Race::Stout):
+		HeroicCreature(1, 0, Size::small, 25);
+		set_ability_score_increase(Ability::dexterity, 2);
+		break;
+
+	case (Race::HalfOrc):
+		HeroicCreature(1, 60, Size::medium, 30);
+		set_ability_score_increase(Ability::constitution, 1);
+		set_ability_score_increase(Ability::strength, 2);
+		break;
+
+	case (Race::Human):
+		HeroicCreature(1, 0, Size::medium, 30);
+		set_ability_score_increase(Ability::charisma, 1);
+		set_ability_score_increase(Ability::constitution, 1);
+		set_ability_score_increase(Ability::dexterity, 1);
+		set_ability_score_increase(Ability::intelligence, 1);
+		set_ability_score_increase(Ability::strength, 1);
+		set_ability_score_increase(Ability::wisdom, 1);
+		break;
+
+	case (Race::Tiefling):
+		HeroicCreature(1, 60, Size::medium, 30);
+		set_ability_score_increase(Ability::charisma, 2);
+		set_ability_score_increase(Ability::intelligence, 1);
+		break;
+	}
+
+	// sub-races specifics
+	switch (race)
+	{
+	case (Race::HillDwarf):
+		set_ability_score_increase(Ability::wisdom, 1);
+		break;
+
+	case (Race::MountainDwarf):
+		set_ability_score_increase(Ability::strength, 2);
+		break;
+
+	case (Race::HighElf):
+		set_ability_score_increase(Ability::intelligence, 1);
+		break;
+
+	case (Race::WoodElf):
+		set_ability_score_increase(Ability::wisdom, 1);
+		break;
+
+	case (Race::DarkElf):
+		set_ability_score_increase(Ability::charisma, 1);
+		break;
+
+	case (Race::ForestGnome):
+		set_ability_score_increase(Ability::dexterity, 1);
+		break;
+
+	case (Race::RockGnome):
+		set_ability_score_increase(Ability::constitution, 1);
+		break;
+
+	case (Race::Lightfoot):
+		set_ability_score_increase(Ability::charisma, 2);
+		break;
+
+	case (Race::Stout):
+		set_ability_score_increase(Ability::constitution, 2);
+		break;
+	}
+
+}
--- src/character/HeroicCreature.ixx
+++ src/character/HeroicCreature.ixx
@@ -3,8 +3,6 @@ module;
 #include <boost/archive/xml_oarchive.hpp>
 #include <boost/archive/xml_iarchive.hpp>
 #include <boost/serialization/version.hpp>
-#include <boost/serialization/map.hpp>
-#include <boost/serialization/vector.hpp>
 
 export module HeroicCreature;
 
@@ -14,54 +12,8 @@ import enumeration;
 export class HeroicCreature : public ICreature
 {
 public:
-	/*
-	HillDwarf, MountainDwarf,
-	Dragonborn,
-	HighElf, WoodElf, DarkElf,
-	ForestGnome, RockGnome,
-	HalfElf,
-	Lightfoot, Stout,
-	HalfOrc,
-	Human,
-	Tiefling
-	*/
 
-	HeroicCreature(Race race = Race::HillDwarf) : m_race(race)
-	{
-		// main races
-		switch (race)
-		{
-		case (Race::HillDwarf):
-		case (Race::MountainDwarf):
-			HeroicCreature(1, 60, Size::medium, 25);
-			set_ability_score_increase(Ability::constitution, 2);
-			add_resistance(Damage::poison);
-			break;
-
-		case (Race::Dragonborn):
-			HeroicCreature(1, 0, Size::medium, 30);
-			set_ability_score_increase(Ability::charisma, 1);
-			set_ability_score_increase(Ability::strength, 2);
-
-		default:
-			throw std::invalid_argument("Unknown race");
-		}
-
-		// sub-races specifics
-		switch (race)
-		{
-		case (Race::HillDwarf):
-			set_ability_score_increase(Ability::wisdom, 1);
-			break;
-
-		case (Race::MountainDwarf):
-			set_ability_score_increase(Ability::strength, 2);
-			break;
-		}
-
-
-
-	}
+	HeroicCreature(Race race = Race::HillDwarf);
 
 	Race race() const { return m_race; }
 
@@ -85,4 +37,4 @@ private:
 	Race m_race;
 };
 
-BOOST_CLASS_VERSION(HeroicCreature, serialization_versions::race_creature)
+BOOST_CLASS_VERSION(HeroicCreature, serialization_versions::heroic_creature)
