0bde9e69b06adfb67fe58ad4398abc264b881b1f
Benoit Triay <benoit.tria+13y@gmail.com>
2022-12-12 21:21:11 +0000

    clean up io::xml::deserialize

--- src/utilities/io.cpp
+++ src/utilities/io.cpp
@@ -40,12 +40,11 @@ void io::xml::serialize(const Hero& hero, const std::string& filename)
 
 Hero io::xml::deserialize(const std::string& filename)
 {
-	Hero hero{"bidule"};
-	//auto race = io::xml::get_class_name(filename, "m_race_name");
-	auto cclass = io::xml::get_class_name(filename, "m_class_name");
+	Hero hero{"Aragorn"};
 
 	hero.set_race_creature(new HeroicCreature);
 
+	auto cclass = io::xml::get_class_name(filename, "m_class_name");
 	if (!cclass.compare("Cleric"))
 		hero.set_class(new Cleric);
 
