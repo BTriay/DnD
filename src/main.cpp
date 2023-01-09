808afea6fab32be91f4f992a51a29a84539cc73d
Benoit Triay <benoit.tria+13y@gmail.com>
2023-01-09 15:23:27 +0000

    main: usage of fire bolt on Voldemort

--- src/main.cpp
+++ src/main.cpp
@@ -37,31 +37,46 @@ int main()
     helgret.add_skill(Skill::medicine);
     
     auto hp = helgret.gain_level(true);
-    std::cout << "Gained " << hp << " hp when levelling up\n";   
+    //std::cout << "Gained " << hp << " hp when levelling up\n";   
     hp = helgret.gain_level(false);
-    std::cout << "Gained " << hp << " hp when levelling up\n";
+    //std::cout << "Gained " << hp << " hp when levelling up\n";
     
+    /*
     auto slots = helgret.available_spell_slots();    
     std::cout << "slots: ";
     for (auto& s : slots)
         std::cout << s << "  ";
     std::cout << '\n';
-
+    */
 
     helgret.restore_current_hp_to_max();
 
     auto armor = armor_creator(ArmorType::chain_mail);
     helgret.don_armor(armor);
 
-    std::cout << "helgret before:\n";
-    std::cout << helgret<< '\n';        
-    io::xml::serialize(helgret, filename);
-    
-    auto helgret_reborn = io::xml::deserialize(filename);        
-    std::cout << "helgret reborn:\n";
-    std::cout << helgret_reborn << '\n';
-    io::xml::serialize(helgret_reborn, filename2);    
-    
+    {
+        AbilityScore voldemort_ability_scores{
+            .charisma = 14, .constitution = 8, .dexterity = 8,
+            .intelligence = 16, .strength = 8, .wisdom = 15 };
+
+        Hero voldemort{ "Voldemort", Race::HillDwarf, "Wizard", voldemort_ability_scores };
+        std::cout << "Voldemort before being attacked\n" << voldemort << '\n';
+        helgret.invoke_spell("fire_bolt", voldemort);
+        std::cout << "Voldemort after being attacked\n" << voldemort << '\n';
+    }
+
+    /*
+    {
+        std::cout << "helgret before:\n";
+        std::cout << helgret << '\n';
+        io::xml::serialize(helgret, filename);
+
+        auto helgret_reborn = io::xml::deserialize(filename);
+        std::cout << "helgret reborn:\n";
+        std::cout << helgret_reborn << '\n';
+        io::xml::serialize(helgret_reborn, filename2);
+    }
+    */
 
     //helgret.add_weapon_one(weapon_creator(WeaponType::warhammer));
     //helgret.don_shield();
